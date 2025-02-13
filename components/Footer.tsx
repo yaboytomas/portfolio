356da820d23fa68314904377c47b0d7da7ac4ed3
Tomas <tomasszabo94@gmail.com>
2025-02-13 15:36:47 -0300

    fixed some bugs on footer and header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,9 @@ import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full mb-[100px] pb-10 md:md-5" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
+
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">

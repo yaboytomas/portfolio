febf8d808b8028702418c736880456ad3c32ed40
Tomas <tomasszabo94@gmail.com>
2025-03-24 14:58:27 -0300

    redirects

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,17 +42,21 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2025 Tomás Szabó. Todos los derechos reservados.
+          Copyright © 2025 Tomás. Todos los derechos reservados.
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
+            <a
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              href={info.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 hover:bg-black-300 transition-colors duration-300"
+              aria-label={info.id === 1 ? 'GitHub' : info.id === 2 ? 'Instagram' : 'LinkedIn'}
             >
               <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+            </a>
           ))}
         </div>
       </div>
